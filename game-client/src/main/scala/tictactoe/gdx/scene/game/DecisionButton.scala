549a0a39871f2e02533c7ea10a48fa587cda4039
Sino <ilyas.alfan.b@gmail.com>
2016-10-26 18:33:03 +0200

    updated grid to hold the sequence of buttons

--- game-client/src/main/scala/tictactoe/gdx/scene/game/DecisionButton.scala
+++ game-client/src/main/scala/tictactoe/gdx/scene/game/DecisionButton.scala
@@ -8,7 +8,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent
 /** A single decision on the grid. */
 private[game] final class DecisionButton(skin: Skin) extends TextButton("", skin) {
   /** The text display to mutate to once selected. */
-  val PleaseWaitText = "Processing..."
+  private val PleaseWaitText = "Processing..."
 
   addListener(new ChangeListener {
     override def changed(event: ChangeEvent, actor: Actor): Unit = {
--- game-client/src/main/scala/tictactoe/gdx/scene/game/DecisionGrid.scala
+++ game-client/src/main/scala/tictactoe/gdx/scene/game/DecisionGrid.scala
@@ -2,16 +2,21 @@ package tictactoe.gdx.scene.game
 
 import com.badlogic.gdx.scenes.scene2d.ui.{Skin, Table}
 
-/** A 9x9 grid of decision buttons. */
+/** A grid of decision buttons. */
 private[game] final class DecisionGrid(skin: Skin) extends Table {
-  val ColumnsPerRow = 3
+  /** The grid size measurements. */
+  private val ColumnsPerRow = 3
+  private val AmountRows = 3
 
-  (1 until 10) foreach { index =>
+  /** The grid mapped to a sequence of buttons. */
+  val buttons = (1 until (ColumnsPerRow * AmountRows) + 1) map { index =>
     val button = new DecisionButton(skin)
     val addedCell = add(button).width(128).height(128) // TODO scalable width/height instead of fixed 128?
     if ((index % ColumnsPerRow) == 0) {
       // 3 columns for each row
       addedCell.row()
     }
+
+    button
   }
 }
