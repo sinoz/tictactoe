fa41257c367f7aa3d9027497664aa3fcc4676a48
Sino <ilyas.alfan.b@gmail.com>
2016-11-24 20:20:18 +0100

    Tile states and updating

--- game-server/src/main/scala/tictactoe/GameBoard.scala
+++ game-server/src/main/scala/tictactoe/GameBoard.scala
@@ -1,16 +1,38 @@
 package tictactoe
 
 import akka.actor.{Actor, ActorLogging, Props}
+import tictactoe.GameBoard.UpdateTo
 
 object GameBoard {
   def apply() =
     Props(new GameBoard)
+
+  /** A message to update the state of a tile. */
+  final case class UpdateTo(id: Int, state: State)
+
+  sealed trait BoardMessage
 }
 
 /** The game board in a single `GameSession` that is affected by two participants / `Player`s. */
 private final class GameBoard extends Actor with ActorLogging {
+  var grid = (0 until 9) map { _ => Tile(Empty) } toArray
+
   override def receive: Receive = {
+    case UpdateTo(id, state) =>
+      grid(id).state = state
+
     case message =>
       unhandled(message)
   }
 }
+
+/** The tile to contain a specific mutable state. */
+final case class Tile(var state: State)
+
+/** The possible tile states. */
+case object X extends State
+case object O extends State
+case object Empty extends State
+
+/** The state of a tile. */
+sealed trait State
