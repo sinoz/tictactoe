3b4636851a7237fd3d40da11d58135e57e114870
Sino <ilyas.alfan.b@gmail.com>
2016-11-24 19:48:11 +0100

    No longer uses Netty externally and instead completely relies on Akka.IO to provide all of our TCP services.

--- game-server/src/main/scala/tictactoe/Server.scala
+++ game-server/src/main/scala/tictactoe/Server.scala
@@ -1,22 +1,33 @@
 package tictactoe
 
+import java.net.InetSocketAddress
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+
 /** The entry point to this game server application. */
-object Server extends App with ServerChannel {
+object Server extends App {
   /** Simple name of the game. */
   val GameName = "TicTacToe"
 
-  /** The default local socket address. */
-  val LocalAddress = localAddress(23192)
+  /** The system of actors and the supervisor actor of this application's hierarchy of actors. */
+  val system = ActorSystem("tictactoe")
+  val server = system actorOf(Props(new Server), "root")
+}
 
-  /** The game internally inside of an `ActorSystem`. */
-  val game = Game.newGame("GameName")
+/** The root supervisor actor within this application. */
+private final class Server extends Actor with ActorLogging {
+  import ServerConstants._
 
-  /** The local hosted server channel. */
-  val serverChannel = newChannel(LocalAddress) { channel =>
-    // TODO: logging?
+  /** The socket channel actor. */
+  val channel = context actorOf(ServerChannel(LocalAddr), "channel")
+
+  override def receive: Receive = {
+    case message =>
+      unhandled(message)
   }
+}
 
-  /** Terminates this game server application. */
-  def terminate() =
-    serverChannel.close()
+/** A mixin that provides a collection of constants. */
+object ServerConstants { // TODO throw this into a package object instead?
+  val LocalAddr = new InetSocketAddress(23192)
 }
--- game-server/src/main/scala/tictactoe/ServerChannel.scala
+++ game-server/src/main/scala/tictactoe/ServerChannel.scala
@@ -1,60 +1,30 @@
 package tictactoe
 
-import java.net.{InetSocketAddress, SocketAddress}
+import java.net.InetSocketAddress
 
-import io.netty.bootstrap.ServerBootstrap
-import io.netty.channel.ChannelHandler.Sharable
-import io.netty.channel.{Channel, ChannelHandlerContext, ChannelInboundHandlerAdapter, ChannelInitializer}
-import io.netty.channel.nio.NioEventLoopGroup
-import io.netty.channel.socket.SocketChannel
-import io.netty.channel.socket.nio.NioServerSocketChannel
-import tictactoe.GameCodec.{PacketDecoder, PacketEncoder}
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.io.Tcp.{Bind, Bound}
+import akka.io.{IO, Tcp}
 
-/** A trait to provide DSL-like functions to simplify local server channel establishments. */
-trait ServerChannel {
-  /** Defines a local socket address. */
-  def localAddress(port: Int) = // TODO perhaps an implicit conversion here?
-    new InetSocketAddress(port)
-
-  /** Defines a new server socket channel using the given address. This is a blocking operation and
-    * should only be called upon startup. */
-  def newChannel(address: SocketAddress)(f: Channel => Unit): Channel = {
-    val bootstrap = new ServerBootstrap()
-
-    bootstrap.channel(classOf[NioServerSocketChannel])
-    bootstrap.group(new NioEventLoopGroup())
-    bootstrap.childHandler(RemoteChannelInitializer)
-    bootstrap.localAddress(address)
-
-    val channel = bootstrap.bind().sync().channel()
-    f(channel)
-    channel
-  }
+object ServerChannel {
+  def apply(address: InetSocketAddress) =
+    Props(new ServerChannel(address))
 }
 
-/** Initializes remote `SocketChannel`s by feeding them the initial pipeline `ChannelHandler`s. */
-object RemoteChannelInitializer extends ChannelInitializer[SocketChannel] {
-  override def initChannel(ch: SocketChannel): Unit = {
-    ch.pipeline().addLast("decoder", new PacketDecoder)
-    ch.pipeline().addLast("encoder", new PacketEncoder)
-    ch.pipeline().addLast("handler", RemoteChannelHandler)
-  }
-}
+/** The game server socket channel bounded to the class injected local address and listens
+  * for incoming connections and data to process. */
+private final class ServerChannel(address: InetSocketAddress) extends Actor with ActorLogging {
+  import context.system
 
-/** Receives and reacts upon remote channel events. */
-@Sharable private object RemoteChannelHandler extends ChannelInboundHandlerAdapter {
-  @throws[Exception]
-  override def channelUnregistered(ctx: ChannelHandlerContext): Unit = {
-    // TODO
+  override def preStart(): Unit = {
+    IO(Tcp) ! Bind(self, address)
   }
 
-  @throws[Exception]
-  override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
-    // TODO
-  }
-}
+  override def receive: Receive = {
+    case Bound(local) =>
+      log.info(s"Bound at $local")
 
-/** A collection of type aliases related to Netty. */
-trait NettyAliases {
-  type MessageOutput = java.util.List[AnyRef]
+    case message =>
+      unhandled(message)
+  }
 }
